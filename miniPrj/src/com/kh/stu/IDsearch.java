987bc29b2c20bdefb070beeb922dfd1490c1e6a5
ParkJoonHyeok <poqwer51@naver.com>
2022-05-26 14:57:20 +0900

    로그인 화면구성@+학생용, 교수용, 관리자용 아이디찾기 화면구성@+학생용, 교수용 비밀번호찾기 화면구성@+학생용, 교수용

---
+++ miniPrj/src/com/kh/stu/IDsearch.java
@@ -0,0 +1,104 @@
+package com.kh.stu;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.kh.util.Util;
+import com.swy.db.OracleDB;
+
+public class IDsearch {
+
+	public static void try_IDFind() {
+		System.out.println("==== 아이디 찾으려는 계정을 선택해주세요. ====");
+		System.out.println("1. 학생  2. 교수");
+		int num = Util.scInt();
+		if (num == 1) {
+			stu_IDFind();
+		} else if (num == 2) {
+			prof_IDFind();
+		} else {
+			System.out.println("잘못 입력 되었습니다.!! ");
+			System.out.println("메인화면으로 이동합니다.");
+		}
+
+	}
+
+	public static void stu_IDFind() {
+		System.out.println("==== 아이디 찾기 ====");
+		System.out.print("이름 : ");
+		String name = Util.sc.nextLine().trim();
+		System.out.print("전화번호 ex)010-0000-0000 : ");
+		String phone = Util.sc.nextLine().trim();
+
+		Connection conn = OracleDB.getConnection();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		// 해당 아이디에 맞는 패스워드 DB에서 조회하기 SELECT = executeQuery();
+		String sql = "SELECT STU_ID FROM STUDENT WHERE STU_NAME = ? AND STU_PHONE = ?";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, name);
+			pstmt.setString(2, phone);
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				String foundID = rs.getString(1);
+				if (foundID == null) {
+					System.out.println("그런 아이디는 존재 하지 않습니다...");
+				} else
+					System.out.println(name + "님의 아이디는 " + foundID);
+			}else {
+				System.out.println("잘못된 정보를 적었습니다.");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("!! SQL 예외 발생 !!");
+		} finally {
+			OracleDB.close(conn);
+			OracleDB.close(pstmt);
+			OracleDB.close(rs);
+		}
+	}
+
+	public static void prof_IDFind() {
+		System.out.println("==== 아이디 찾기 ====");
+		System.out.print("이름 : ");
+		String name = Util.sc.nextLine().trim();
+		System.out.print("전화번호 ex)010-0000-0000 : ");
+		String phone = Util.sc.nextLine().trim();
+
+		Connection conn = OracleDB.getConnection();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		// 해당 아이디에 맞는 패스워드 DB에서 조회하기 SELECT = executeQuery();
+		String sql = "SELECT P_ID FROM PROF WHERE P_NAME = ? AND P_PHONE = ?";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, name);
+			pstmt.setString(2, phone);
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				String foundID = rs.getString(1);
+				if (foundID == null) {
+					System.out.println("그런 아이디는 존재 하지 않습니다...");
+				} else
+					System.out.println(name + "님의 아이디는 " + foundID);
+			}else {
+				System.out.println("잘못된 정보를 적었습니다.");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("!! SQL 예외 발생 !!");
+		} finally {
+			OracleDB.close(conn);
+			OracleDB.close(pstmt);
+			OracleDB.close(rs);
+		}
+	}
+
+}
--- miniPrj/src/com/kh/stu/Login.java
+++ miniPrj/src/com/kh/stu/Login.java
@@ -5,60 +5,148 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-
 import com.kh.util.Util;
 import com.swy.db.OracleDB;
 
-
 public class Login {
 
-	public static int stu_Login() {
-		
+	// 선별
+	public static void try_login() {
+		System.out.println("==== 로그인 하려는 계정을 선택해주세요. ====");
+		System.out.println("1. 학생  2. 교수  3. 관리자");
+		int num = Util.scInt();
+		if (num == 1) {
+			stu_Login();
+		}
+		if (num == 2) {
+			prof_Login();
+		}
+		if (num == 3) {
+			admin_Login();
+		}
+
+	}
+
+	// 학생 로그인
+	public static void stu_Login() {
 		System.out.println("==== 로그인 하기 ====");
 		System.out.print("아이디 : ");
 		String id = Util.sc.nextLine();
 		System.out.print("비밀번호 : ");
 		String pwd = Util.sc.nextLine();
-		
-		
-		//DB연결
+
+		// DB연결
 		Connection conn = OracleDB.getConnection();
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		
-		//해당 아이디에 맞는 패스워드 DB에서 조회하기 SELECT  =  executeQuery();
+
+		// 해당 아이디에 맞는 패스워드 DB에서 조회하기 SELECT = executeQuery();
 		String sql = "SELECT STU_PWD FROM STUDENT WHERE STU_ID = ?";
-		
+
 		try {
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, id);
 			rs = pstmt.executeQuery();
-			
-			if(rs.next()) {
+
+			if (rs.next()) {
 				String dbPwd = rs.getString(1);
-				if(dbPwd.equals(pwd)) {
-					//로그인 성공
+				if (dbPwd.equals(pwd)) {
+					// 로그인 성공
 					System.out.println("로그인 성공!!!");
-					return 1;
-				}else {
-					System.out.println("비밀번호를 잘못 입력하였습니다!");
-					return 0;	
-				}
-			}
+					stuMenu.showMenu();
+				} 
+			}else
+				System.out.println("로그인 실패!");
 		} catch (SQLException e) {
 			e.printStackTrace();
-			System.out.println("!! SQL 예외 발생 !!");	
-			return -1;
-		}finally {
+			System.out.println("!! SQL 예외 발생 !!");
+		} finally {
 			OracleDB.close(conn);
 			OracleDB.close(pstmt);
 			OracleDB.close(rs);
 		}
-		
-		System.out.println("로그인 실패 입니다...");
-		return -2;
-		
-		
+	}
+
+	// 교수 로그인
+	public static void prof_Login() {
+		System.out.println("==== 로그인 하기 ====");
+		System.out.print("아이디 : ");
+		String id = Util.sc.nextLine();
+		System.out.print("비밀번호 : ");
+		String pwd = Util.sc.nextLine();
+
+		// DB연결
+		Connection conn = OracleDB.getConnection();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		// 해당 아이디에 맞는 패스워드 DB에서 조회하기 SELECT = executeQuery();
+		String sql = "SELECT P_PWD FROM PROF WHERE P_ID = ?";
+
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				String dbPwd = rs.getString(1);
+				if (dbPwd.equals(pwd)) {
+					// 로그인 성공
+					System.out.println("로그인 성공!!!");
+					System.out.println("교수 메뉴 이동");
+				} 
+			}else
+				System.out.println("로그인 실패!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("!! SQL 예외 발생 !!");
+		} finally {
+			OracleDB.close(conn);
+			OracleDB.close(pstmt);
+			OracleDB.close(rs);
+		}
+	}
+
+	// 관리자 로그인
+	public static void admin_Login() {
+		System.out.println("==== 로그인 하기 ====");
+		System.out.print("아이디 : ");
+		String id = Util.sc.nextLine();
+		System.out.print("비밀번호 : ");
+		String pwd = Util.sc.nextLine();
+
+		// DB연결
+		Connection conn = OracleDB.getConnection();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		// 해당 아이디에 맞는 패스워드 DB에서 조회하기 SELECT = executeQuery();
+		String sql = "SELECT AD_PWD FROM ADMIN WHERE AD_ID = ?";
+
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				String dbPwd = rs.getString(1);
+				if (dbPwd.equals(pwd)) {
+					// 로그인 성공
+					System.out.println("로그인 성공!!!");
+					System.out.println("관리자 메뉴 이동");
+				} 
+			}else
+				System.out.println("로그인 실패!");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("!! SQL 예외 발생 !!");
+		} finally {
+			OracleDB.close(conn);
+			OracleDB.close(pstmt);
+			OracleDB.close(rs);
+		}
 	}
 	
+	
 }
+
