f6c55c5e07a475de386c68ef07b18e2cab47a55d
DESKTOP-3KDMSRL\ssora <ssora0420@hufs.ac.kr>
2022-05-26 15:00:53 +0900

    회원가입 화면 작업 진행중. 비밀번호 확인 작업 진행중

---
+++ miniPrj/src/com/kh/stu/stuSignMenu.java
@@ -0,0 +1,12 @@
+package com.kh.stu;
+
+public class stuSignMenu {
+
+	public static void main(String[] args) {
+		System.out.println("==== KH 대학교 수강신청 홈페이지 ====");
+		System.out.println("1. 회원가입");
+		System.out.println("2. 나가기");
+
+	}
+
+}
