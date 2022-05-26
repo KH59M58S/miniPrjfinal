aab2aedb70616f03be173568a54e091c80ea2aa6
DESKTOP-JNI98I1\chan <chanrb0966@gmail.com>
2022-05-26 14:54:43 +0900

    수강신청 페이지 제작 수강신청 디비 약간 수정

---
+++ miniPrj/src/com/kh/lecture/LSignUpUI.java
@@ -0,0 +1,63 @@
+package com.kh.lecture;
+
+import com.kh.util.Util;
+
+public class LSignUpUI {
+
+	static FindLecture fl = new FindLecture();
+
+	// 수강신청 페이지
+	public static void LSingUpUIStart(int std_no) {		
+
+		while (true) {
+			System.out.println();
+			System.out.println("++++++++++ 수강신청 ++++++++++");
+			SignUp.showAllLectureList();
+			System.out.println("++++++++++++++++++++++++++++");
+			System.out.println("1. 수강신청");
+			System.out.println("2. 강의명별 검색");
+			System.out.println("3. 교수명별 검색");
+			System.out.println("0. 이전 단계");
+			System.out.println("++++++++++++++++++++++++++++");
+			System.out.print("입력창 : ");
+			int chose = Util.scInt();
+			if (chose == 0) {
+				System.out.println("이전 페이지...");
+				break;
+			} else if (chose == 1) {
+				while (true) {
+					System.out.print("신청할 강의번호 입력(이전 단계 = 0) : ");
+					int c_no = Util.scInt();
+
+					if (c_no == 0) {
+						System.out.println("이전 페이지...");
+						break;
+					}
+					if (SignUp.insertSignUp(c_no, std_no)) {
+						System.out.println("신청완료!!!");
+					} else {
+						System.out.println("잘못 입력하였습니다.");
+					}
+				}
+			} else if (chose == 2) {
+				System.out.println("강의별 검색입니다.");
+				// 강의별 검색 나둥에 만들면 넣기++++++++++++++++++++++++++++++++
+			} else if (chose == 3) {
+				System.out.println("교수별 검색입니다.");
+			}
+		}
+	}
+
+	//장바구니 기능
+	public static void userSingUpList(int std_no) {
+		System.out.println("++++++++++++ 내가 신청한 강의 +++++++++++++");
+		SignUp.showStdSignUp(std_no);
+	}
+
+	
+	//테스트용 메인메서드 나중에 없앰
+	public static void main(String[] args) {
+		LSingUpUIStart(1);
+	}
+
+}
--- miniPrj/src/com/kh/lecture/SignUp.java
+++ miniPrj/src/com/kh/lecture/SignUp.java
@@ -44,16 +44,15 @@ public class SignUp {
 	}
 
 	// 수강신청
-	public static boolean insertSignUp(int s_no, int c_no, int stu_no) {
+	public static boolean insertSignUp(int c_no, int stu_no) {
 		Connection conn = OracleDB.getConnection();
 		PreparedStatement pstmt = null;
 		String sql = "INSERT INTO SIGNUP (S_NO, C_NO, STU_NO)" 
-				+ "VALUES(?,?,?)";
+				+ "VALUES(SIGNUP_NO.NEXTVAL,?,?)";
 		try {
 			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, s_no);
-			pstmt.setInt(2, c_no);
-			pstmt.setInt(3, stu_no);
+			pstmt.setInt(1, c_no);
+			pstmt.setInt(2, stu_no);
 
 			int resualt = pstmt.executeUpdate();
 			if (resualt == 1) {
@@ -72,7 +71,7 @@ public class SignUp {
 	}
 
 	// 학생별 신청내역 검색
-	public static void showStdSignUp(String std_name) {
+	public static void showStdSignUp(int std_no) {
 		Connection conn = OracleDB.getConnection();
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -82,12 +81,11 @@ public class SignUp {
 				+ " ON S.STU_NO = STU.STU_NO" 
 				+ " INNER JOIN CLASS C"
 				+ " ON S.C_NO = c.c_no" 
-				+ " WHERE S.S_QUIT_YN = 'N' AND STU.STU_NAME = '" + std_name + "'";
+				+ " WHERE S.S_QUIT_YN = 'N' AND STU.STU_NO = '" + std_no + "'";
 		try {
 			pstmt = conn.prepareStatement(sql);
 			rs = pstmt.executeQuery(sql);
 
-			
 			while (rs.next()) {
 				System.out.println(
 						"이름:" + rs.getString(1) + "  " + "강의명:" + rs.getString(2) + "  " + "신청일자:" + rs.getDate(3));
@@ -139,13 +137,35 @@ public class SignUp {
 				OracleDB.close(rs);
 		}
 	}
+	
+	public static void showAllLectureList() {
+		Connection conn = OracleDB.getConnection();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		String sql = "SELECT C.C_NO, C_NAME, C.C_TIME, C.C_ROOM, P.P_NAME "
+				+ "FROM CLASS C "
+				+ "INNER JOIN PROF P "
+				+ "ON (C.P_NO = P.P_NO) ";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery(sql);
 
-//	public static void main(String[] args) {
-//		System.out.println("start");
-//		showAllSignupList();
-//		System.out.println(insertSignUp(1, 1, 1));
-//		showStdSignUp("박주은");
-//		showLectureSingUp("A");
-//	}
+			while (rs.next()) {
+				System.out.println("강의번호:" + rs.getString(1) + "  강의명:" + rs.getString(2) 
+								+ "  시간:"+ rs.getString(3) + "  강의실:"+ rs.getString(4) + "  교수명:" + rs.getString(5));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (conn == null)
+				OracleDB.close(conn);
+			if (pstmt == null)
+				OracleDB.close(pstmt);
+			if (rs == null)
+				OracleDB.close(rs);
+		}
+	}
 
 }
